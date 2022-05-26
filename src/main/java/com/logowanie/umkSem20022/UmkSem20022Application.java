cb5bc6e714dd1e6563e13eb2065d5824ac8511a0
marooo-o <marroskox@gmail.com>
2022-05-26 10:44:54 +0200

    init kurna

---
+++ src/main/java/com/logowanie/umkSem20022/UmkSem20022Application.java
@@ -0,0 +1,13 @@
+package com.logowanie.umkSem20022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmkSem20022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmkSem20022Application.class, args);
+	}
+
+}
