3c34f6b9e92596d71006dc719cfe4cbb7dba1f27
marooo-o <marroskox@gmail.com>
2022-07-31 14:40:16 +0200

    To fix: AuthenticationRequest pulling null email field

--- springboot/src/main/java/com/logowanie/umkSem20022/Logowanie/Repositories/UserRepository.java
+++ springboot/src/main/java/com/logowanie/umkSem20022/Logowanie/Repositories/UserRepository.java
@@ -7,10 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends MongoRepository<UserModel, String> {
 
-    User findByEmailAndPassword(String email, String[] password);
-    @Query("{email:'?0'}")
-    User findByEmail(String email);
-
     UserModel findUserModelByEmail(String email);
 
 
